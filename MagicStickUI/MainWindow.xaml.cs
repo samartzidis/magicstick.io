87b70b4c0b2df3f1f50b4d9b045acc57c2476fde
samartzidis <samartzidis@gmail.com>
2023-10-05 20:37:07 +0100

    -

--- MagicStickUI/MainWindow.xaml.cs
+++ MagicStickUI/MainWindow.xaml.cs
@@ -66,8 +66,6 @@ namespace MagicStickUI
                     UpdateDevice(SelectedDevice);
             });
             _updateTimer.Start();
-
-            throw new Exception("Ooops!");
         }
 
         
